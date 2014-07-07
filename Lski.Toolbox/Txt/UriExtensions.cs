02897f508d81d719d632d876d14c632878330cba
Lee Cooper <lee.cooper@lski.uk>
2014-07-07 10:22:50 +0100

    Added UriExtensions class for adding parameters

---
+++ Lski.Toolbox/Txt/UriExtensions.cs
@@ -0,0 +1,37 @@
+﻿namespace System {
+
+	public static class UriExtensions {
+
+		/// <summary>
+		/// Creates a new uri string from an existing Uri object, where a parameter has been added. Non-destructive.
+		/// </summary>
+		/// <param name="uri"></param>
+		/// <param name="name"></param>
+		/// <param name="value"></param>
+		/// <returns></returns>
+		public static string AddParameter(this Uri uri, string name, object value) {
+
+			if (uri == null) {
+				throw new ArgumentNullException("uri");
+			}
+			if (name == null) {
+				throw new ArgumentNullException("name");
+			}
+
+			var url = uri.GetLeftPart(UriPartial.Query) + (uri.Query.Length > 0 ? "&" : "?") + String.Format("{0}={1}", name, value);
+
+			return url;
+		}
+
+		/// <summary>
+		/// Creates a new uri string from an existing Uri, where a parameter has been added. Non-destructive.
+		/// </summary>
+		/// <param name="uri"></param>
+		/// <param name="name"></param>
+		/// <param name="value"></param>
+		/// <returns></returns>
+		public static string AddParameter(string uri, string name, object value) {
+			return AddParameter(new Uri(uri), name, value);
+		}
+	}
+}
