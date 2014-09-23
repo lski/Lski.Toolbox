272bc49ea65168f63535e2e7313166bd94dac1e7
Lee Cooper <lee.cooper@lski.uk>
2014-09-23 16:57:32 +0100

    Handled the potential of a null value being placed into value in the AddParameter method

--- Lski.Toolbox/Txt/UriExtensions.cs
+++ Lski.Toolbox/Txt/UriExtensions.cs
@@ -11,7 +11,9 @@ namespace System {
 		/// <summary>
 		/// Creates a new Uri with the added name/value pair added to the end of the Uri.
 		///
-		/// This method creates a new Uri and is non-destructive and uses String.Format to covert the value to a string before encoding the name and value for use in a Url
+		/// This method creates a new Uri and is non-destructive and uses String.Format to covert the value to a string before encoding the name and value for use in a Url.
+		/// 
+		/// Important: If value is null then it does not added the name to the string and returns the uri unchanged
 		/// </summary>
 		/// <returns></returns>
 		public static Uri AddParameter(this Uri uri, string name, object value) {
@@ -24,6 +26,10 @@ namespace System {
 				throw new ArgumentNullException("name");
 			}
 
+			if (value == null) {
+				return uri;
+			}
+
 			var param = WebUtility.UrlEncode(name) + "=" + WebUtility.UrlEncode(String.Format("{0}", value));
 			var qry = uri.Query + (uri.Query.Length == 0 ? "?" : "&") + param;
 			var lnk = uri.GetLeftPart(UriPartial.Path) + qry + uri.Fragment;
@@ -34,7 +40,9 @@ namespace System {
 		/// <summary>
 		/// Creates a new Uri with the added name/value pair added to the end of the Uri.
 		///
-		/// This method creates a new Uri and is non-destructive and uses String.Format to covert the value to a string before encoding the name and value for use in a Url
+		/// This method creates a new Uri and is non-destructive and uses String.Format to covert the value to a string before encoding the name and value for use in a Url.
+		/// 
+		/// Important: If value is null then it does not added the name to the string and returns the uri unchanged
 		/// </summary>
 		/// <returns></returns>
 		public static string AddParameter(string uri, string name, object value) {
