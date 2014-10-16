11b6a9d03cf0a33a8bc7702814bf6024a73baf40
Lee Cooper <lee.cooper@lski.uk>
2014-10-16 17:06:18 +0100

    Updated the nuget package

--- src/Lski.Toolbox/Properties/AssemblyInfo.cs
+++ src/Lski.Toolbox/Properties/AssemblyInfo.cs
@@ -6,4 +6,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: ComVisible(false)]
 [assembly: Guid("a04e05b0-220c-4a98-9b7f-b5cac99d19e6")]
-[assembly: AssemblyVersion("1.0.1.*")]
+[assembly: AssemblyVersion("1.0.2.*")]
