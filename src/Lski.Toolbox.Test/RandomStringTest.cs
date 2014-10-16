69120937a33d41488f363006a9c82184596c5d27
Lee Cooper <lee.cooper@lski.uk>
2014-10-16 17:01:10 +0100

    Improved the useability of the RandomString class and added a couple of simple tests

---
+++ src/Lski.Toolbox.Test/RandomStringTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Lski.Toolbox.Txt;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace Lski.Toolbox.Test {
+
+	[TestClass]
+	public class RandomStringTest {
+
+		[TestMethod]
+		public void RandomStringsAreDifferent() {
+
+			var r = new RandomString();
+
+			var s1 = r.Generate();
+			var s2 = r.Generate();
+
+			Console.WriteLine(s1);
+			Console.WriteLine(s2);
+
+			s1.Should().NotBe(s2);
+		}
+
+		[TestMethod]
+		public void StringLengthCorrect() {
+
+			var r = new RandomString();
+
+			var s1 = r.Generate();
+
+			Console.WriteLine(s1);
+
+			s1.Should().HaveLength(10);
+
+			s1 = r.Generate(12);
+
+			Console.WriteLine(s1);
+
+			s1.Should().HaveLength(12);
+		}
+	}
+}
