5784c3afb9085319342b4ed52c3d23774cf3c231
lski <lee.cooper@lski.uk>
2016-11-16 21:37:15 +0000

    Synced the assembly numbers to correct last publish

--- src/Lski.Toolbox/Properties/AssemblyInfo.cs
+++ src/Lski.Toolbox/Properties/AssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Lee Cooper")]
 [assembly: ComVisible(false)]
 [assembly: Guid("a04e05b0-220c-4a98-9b7f-b5cac99d19e6")]
-[assembly: AssemblyInformationalVersion("3.0.0")]
-[assembly: AssemblyVersion("3.0.0.*")]
+[assembly: AssemblyInformationalVersion("3.3.1")]
+[assembly: AssemblyVersion("3.3.1.*")]
