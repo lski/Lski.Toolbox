cd45e4c659c43fdcc53941106e1cf9470d38600b
lski <lee.cooper@lski.uk>
2017-01-24 19:47:38 +0000

    Added ToIso8601 on DateTimeExt class

--- src/Lski.Toolbox/Dates/DateTimeExt.cs
+++ src/Lski.Toolbox/Dates/DateTimeExt.cs
@@ -1,27 +1,27 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Lski.Toolbox.Dates {
 
     public static class DateTimeExt {
 
-        public const String IsTime = @"^(20|21|22|23|[01]\d|\d)[:](([0-5]\d){1,2})$";
-        public const String EndsWithTime = "^" + IsTime;
+        private const string IsoDateTimeFormat = "{0:0000}-{1:00}-{2:00}T{3:00}:{4:00}:{5:00}Z";
+        private const String IsTime = @"^(20|21|22|23|[01]\d|\d)[:](([0-5]\d){1,2})$";
+        private const String EndsWithTime = "^" + IsTime;
 
-        private static readonly DateTime _epoch;
-
-        public static DateTime Epoch {
-            get { return _epoch; }
-        }
+        /// <summary>
+        /// A DateTime that represents Epoch
+        /// </summary>
+        public static DateTime Epoch { get; }
 
         static DateTimeExt() {
-            _epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            Epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
         }
 
         /// <summary>
         /// Converts a date into the number of seconds from Epoch. This is done using te UTC version of the time.
         /// </summary>
         /// <param name="date"></param>
-        /// <returns></returns>
         public static long ToUnixTime(this DateTime date) {
 
             return (long)((date.ToUniversalTime() - Epoch).TotalSeconds);
@@ -32,24 +32,28 @@ namespace Lski.Toolbox.Dates {
         ///
         /// Returns a DateTime UTC object, to get a local version call FromUnixTime().ToLocalTime() on the result.
         /// </summary>
-        /// <param name="date"></param>
-        /// <returns></returns>
-        public static DateTime FromUnixTime(this int date) {
-
-            return Epoch.AddSeconds(date);
-        }
+        /// <param name="secondsSinceEpoch"></param>
+        public static DateTime FromUnixTime(this int secondsSinceEpoch) => Epoch.AddSeconds(secondsSinceEpoch);
 
         /// <summary>
         /// Converts a number into a dateTime where the number is the number of seconds since the Epoch.
         ///
         /// Returns a DateTime UTC object, to get a local version call FromUnixTime().ToLocalTime() on the result.
         /// </summary>
-        /// <param name="date"></param>
-        /// <returns></returns>
-        public static DateTime FromUnixTime(this long date) {
+        /// <param name="secondsSinceEpoch"></param>
+        public static DateTime FromUnixTime(this long secondsSinceEpoch) => Epoch.AddSeconds(secondsSinceEpoch);
 
-            return Epoch.AddSeconds(date);
-        }
+        /// <summary>
+        /// Convert a DateTime to a basic ISO8601 date string, does not include offset or milliseconds.
+        /// </summary>
+        /// <param name="date"></param>
+        public static string ToIso8601(this DateTime date) => string.Format(IsoDateTimeFormat, date.Year, date.Month, date.Day, date.Hour, date.Minute, date.Second);
+
+        /// <summary>
+        /// Convert a string in ISO8601 date format to a date time
+        /// </summary>
+        /// <param name="date"></param>
+        public static DateTime FromIso8601(this string date) => DateTime.ParseExact(date.Replace("T", " "), "u", CultureInfo.InvariantCulture);
 
         /// <summary>
         /// Returns new a date, comprising the first day of the week of the date passed, according to the current culture.
@@ -73,7 +77,7 @@ namespace Lski.Toolbox.Dates {
         /// <param name="date">The DateTime object containing the date section desired</param>
         /// <param name="time">The DateTime object containing the time section desired</param>
         /// <param name="includeSeconds">Optional: States whether the new DateTime should include the seconds from the time object or reset to zero</param>
-        /// <returns></returns>
+        [Obsolete("Will be removed in the next major release")]
         public static DateTime CombineWithTime(this DateTime date, DateTime time, Boolean includeSeconds = false) {
 
             return new DateTime(date.Year, date.Month, date.Day, time.Hour, time.Minute, (includeSeconds ? time.Second : 0));
--- src/Lski.Toolbox/Properties/AssemblyInfo.cs
+++ src/Lski.Toolbox/Properties/AssemblyInfo.cs
@@ -1,11 +1,8 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Lski.Toolbox")]
 [assembly: AssemblyProduct("Lski.Toolbox")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyCompany("Lee Cooper")]
-[assembly: ComVisible(false)]
-[assembly: Guid("a04e05b0-220c-4a98-9b7f-b5cac99d19e6")]
-[assembly: AssemblyInformationalVersion("3.3.1")]
-[assembly: AssemblyVersion("3.3.1.*")]
+[assembly: AssemblyInformationalVersion("3.4.1")]
+[assembly: AssemblyVersion("3.4.1.*")]
