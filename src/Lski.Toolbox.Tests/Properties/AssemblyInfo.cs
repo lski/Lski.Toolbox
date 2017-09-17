866870d46ac69f5a873ac16d1d5c9ef53b55ef29
Lee Cooper <lee.cooper@lski.uk>
2017-09-17 12:00:35 +0100

    Use Cli 2+ to combine 4.5 and 1.4 builds

--- src/Lski.Toolbox.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Lski.Toolbox.Test")]
-[assembly: AssemblyProduct("Lski.Toolbox.Test")]
-[assembly: AssemblyVersion("1.0.0")]
--- src/Lski.Toolbox/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Lski.Toolbox")]
-[assembly: AssemblyProduct("Lski.Toolbox")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyCompany("Lee Cooper")]
-[assembly: AssemblyInformationalVersion("4.4.0")]
-[assembly: AssemblyVersion("4.4.0.*")]
